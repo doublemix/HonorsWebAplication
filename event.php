449c0fe1208adcb32aa8aa31b5a408ab73f2f987
Mitch <doublemx2@gmail.com>
2018-04-10 23:14:21 -0400

    Added RSVP List for Admin on Event Page.

--- event.php
+++ event.php
@@ -30,6 +30,10 @@ $loggedIn = getPermissions($conn);
 
 if ($loggedIn) {
     $userId = ((int) $_SESSION["userid"]);
+    $isAdmin = hasPermission(PERM_ADMIN);
+} else {
+    $userId = null;
+    $isAdmin = false;
 }
 
 $academiceventsTable = DB_getPrefixedTable("academicevents");
@@ -46,8 +50,9 @@ $cce = DB_executeAndFetchOne($sql);
 
 $cceStart = \DateTimeImmutable::createFromFormat('Y-m-d H:i:s', $cce['start_date']);
 $now = new \DateTimeImmutable();
+
 // if logged in and is not an admin (the admin cannot register for events) and time has not passed
-if ($loggedIn && $_SESSION["permissions"][PERM_ADMIN] != 1 && $cceStart > $now) {
+if ($loggedIn && !$isAdmin && $cceStart > $now) {
 
     $canRsvp = true;
 
@@ -77,12 +82,35 @@ if ($loggedIn) {
     $isAttend = false;
 }
 
+if ($isAdmin) {
+    $rsvpTable = DB_getPrefixedTable("cce_rsvp");
+    $studentsTable = DB_getPrefixedTable("currentstudents");
+    $attendanceSheet = DB_executeAndFetchAll(
+        "SELECT "
+        . "CONCAT(users.usr_fname, ' ', users.usr_lname) AS `name` "
+        . "FROM $rsvpTable rsvp "
+        . "JOIN $studentsTable pstu ON rsvp.pstu_id = pstu.pstu_id "
+        . "JOIN users ON pstu.pstu_id = users.usr_id "
+        . "WHERE rsvp.acad_id = $eventId "
+        . "ORDER BY `name`"
+    );
+}
+
 HTML_StartHead();
 
 AddTitle($cce['title']);
 
 AddCSS("all.css");
-AddCSS("bootstrap.min.css");
+AddCSS("jquery-ui.min.css");
+echo "<link rel=\"stylesheet\" href=\"https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css\" integrity=\"sha384-1q8mTJOASx8j1Au+a5WDVnPi2lkFfwwEAa8hDDdjZlpLegxhjVME1fgjWPGmkzs7\" crossorigin=\"anonymous\">";
+echo "<link rel=\"stylesheet\" href=\"https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap-theme.min.css\" integrity=\"sha384-fLW2N01lMqjakBkx3l/M9EahuwpSfeNvV63J5ezn3uZzapT0u7EYsXMjQV+0En5r\" crossorigin=\"anonymous\">";
+AddCSS("jquery.bootgrid.min.css");
+
+Body_AddScript("jquery-1.12.3.min.js");
+Body_AddScript("jquery-ui.min.js");
+Body_AddScript("bootstrap.min.js");
+Body_AddScript("jquery.bootgrid.min.js");
+Body_AddScript("all.js");
 
 HTML_EndHead();
 
@@ -155,14 +183,37 @@ Body_CreateStickyNav();
     if ($isAttend) {
         ?><p class="alert-box">You have attended this event.</p><?php
     }
-    ?><p><?php echo htmlspecialchars($cce["desc"]) ?></p>
+    ?><p><?php echo htmlspecialchars($cce["desc"]) ?></p><?php
+    if (isset($attendanceSheet)) { ?>
+        <h3>RSVP List</h3>
+        <table id="rsvp_table" class="table table-hover table-striped">
+            <thead>
+                <tr>
+                    <th data-column-id="name" data-converter="string">
+                        Name
+                    </th>
+                </tr>
+            </thead>
+        </table>
+        <script type="text/javascript">
+            const data = <?php echo json_encode($attendanceSheet); ?>;
+
+            $(document).ready(function () {
+                const rsvpTable = $("#rsvp_table");
+                rsvpTable.bootgrid({
+                    data: data,
+                    columnSelection: false,
+                    rowCount: -1, // display all
+                    caseSensitive: false,
+                });
+                rsvpTable.bootgrid("append", data);
+                rsvpTable.bootgrid("sort", {name: "asc"});
+            });
+        </script>
+    <?php } ?>
 </div>
 
 <?php
-
-Body_AddScript("jquery-1.12.3.min.js");
-Body_AddScript("bootstrap.min.js");
-Body_AddScript("all.js");
 
 HTML_End();
 
